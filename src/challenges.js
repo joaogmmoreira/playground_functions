2e2d5ddcb3b52f3de41e0402b313b02487966b96
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-12 15:21:53 -0700

    desafios 6 e 8 incompletos

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,4 @@
-// Desafio 1
+// Desafio 1 COMPLETO!
 function compareTrue(valueA, valueB) {
   // seu código aqui
   if (valueA === true && valueB === true) {
@@ -8,20 +8,20 @@ function compareTrue(valueA, valueB) {
   }
 }
 
-// Desafio 2
+// Desafio 2 COMPLETO!
 function calcArea(base, height) {
   // seu código aqui
   return (base * height)/2
 }
 
-// Desafio 3
+// Desafio 3 COMPLETO!
 function splitSentence(string) {
   // seu código aqui
  
     return (string.split(' '));
   }
 
-// Desafio 4
+// Desafio 4 COMPLETO!
 function concatName(arrStrings) {
   // seu código aqui
   for (let i = 0; i <= arrStrings.length; i ++){
@@ -29,25 +29,53 @@ function concatName(arrStrings) {
   }
 }
 
-// Desafio 5
+// Desafio 5 COMPLETO!
 function footballPoints(wins, ties) {
   // seu código aqui
   return wins*3 + ties;
 }
 
-// Desafio 6
-function highestCount() {
+// Desafio 6 INCOMPLETO
+function highestCount(arr) {
   // seu código aqui
 }
+
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if (Math.abs(cat1 - mouse)> Math.abs(cat2 - mouse)){
+    return 'cat2';
+  
+  }else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return 'cat1';
+
+  }else{
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
-// Desafio 8
-function fizzBuzz() {
+// Desafio 8 INCOMPLETO
+function fizzBuzz(arr) {
   // seu código aqui
+  retorno = [];
+  for (let index = 0; index < arr.length; index ++){
+
+    if (arr[index] % 3 === 0 && arr[index] % 5 !== 0){
+      retorno.push('fizz');
+      
+    }else if (arr[index] % 3 !== 0 && arr[index] % 5 === 0){
+      retorno.push('buzz');      
+
+    }else if (arr[index] % 3 === 0 && arr[index] % 5 === 0){
+      retorno.push('fizzbuzz');
+      retorno.pop();
+
+    }else {
+      retorno.push('bug!');
+    }
+  }
+  return retorno;
 }
 
 // Desafio 9
