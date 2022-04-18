4b12db92f97be362631e38285d8f2aafb3640ed5
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-18 09:36:34 -0700

    desafio13

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,10 +15,52 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(str) {
   // seu código aqui
- 
+  let pattern = /[0-9]/g;
+  let result = str.match(pattern);
+  
+  //console.log(typeof result);
+  
+  for (let i = 0; i < result.length; i++) {
+    result[i] = parseInt(result[i]);    
+    //console.log(result);
+      
+  }
+  var sum = 0;
+  for (let i = 0; i< result.length; i ++) {
+    sum = sum + result[i];
+    //console.log(sum);
+
+  }
+    
+  if (sum > 1) {
+    return sum + ' copos de água';
+
+  } else {
+    return sum + ' copo de água';
+  }
 }
+
+
+
+
+
+
+
+
+  /*newArr = str.split(' ');
+  console.log(newArr);
+  for (let i = 0; i < newArr.length; i ++) {
+    newArr[i] = parseInt(newArr[i]);
+    //console.log(newArr);
+    console.log(newArr2);
+  }
+  return console.log('a soma é' + sum);*/
+
+    
+
+hydrate("1 cachaça, 5 cervejas e 1 copo de vinho");
 
 module.exports = {
   generatePhoneNumber,
